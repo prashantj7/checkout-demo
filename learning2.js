72ad164c035d122ceef3a21f03a821069171236d
prashant-coditas <prashant.jadhav@coditas.com>
2018-08-10 18:47:46 +0530

    added too many changes

--- learning2.js
+++ learning2.js
@@ -1,1 +1,6 @@
 document.write("A");
+
+console.log("I have made important changes into this file");
+
+console.log("I have made important changes into this file");console.log("I have made important changes into this file");
+console.log("I have made important changes into this file");
